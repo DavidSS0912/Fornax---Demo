df9cd56b3be487c54a90f58c66aa817659fdf846
David SZ <david-sz@example.com>
2026-07-04 13:22:25 -0700

    Fix base path in config to match repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fornax-demo/',
+  base: '/Fornax---Demo/',
   plugins: [
     react(),
     tailwindcss()
